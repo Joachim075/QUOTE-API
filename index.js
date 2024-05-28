3274c91d6be2cc72ba214f0cdd51b8736a3c7fd5
Kafuluma Joachim <144176451+Joachim075@users.noreply.github.com>
2024-05-28 13:56:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-import app from "./server.js"
+import express  from "express";
+import morgan from "morgan";
+import cors from "cors"
+import { autherRouter } from "./routes/auther-router.js";
+import { qouteRouter } from "./routes/quote-router.js";
+
+const app= express();
+
+const PORT= process.env.PORT || 4000;
+
+//middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/",autherRouter)
+app.use("/",qouteRouter)
 
 
 
-app.listen(1000,()=>{ console.log("server is on port 1000")})
+
+app.listen(1000,()=>{ console.log(`server is running on port ${PORT}`)})
 
