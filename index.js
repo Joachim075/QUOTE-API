b889cee21f0d64fad5be27607731e322df060379
Kafuluma Joachim <144176451+Joachim075@users.noreply.github.com>
2024-05-28 15:41:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-import express  from "express";
+import express from "express";
 import morgan from "morgan";
-import cors from "cors"
+import cors from "cors";
 import { autherRouter } from "./routes/auther-router.js";
 import { qouteRouter } from "./routes/quote-router.js";
 
-const app= express();
+const app = express();
 
-const PORT= process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-//middleware
+// Middleware
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.use("/",autherRouter)
-app.use("/",qouteRouter)
+// Redirect root URL to autherRouter
+app.get("/", (req, res) => {
+  res.redirect("/auther");
+});
 
+// Routes
+app.use("/auther", autherRouter);
+app.use("/quote", qouteRouter);
 
-
-
-app.listen(1000,()=>{ console.log(`server is running on port ${PORT}`)})
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
